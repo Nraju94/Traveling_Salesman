5f4d5865b1b8c51ef792bb6b642c0eb104bb9da4
Nraju94 <Nra1ju94#>
2022-05-21 00:10:26 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+@author: Raj Gouravelli
+Student Id : OD51572
+"""
+
+from services.workflow import Workflow
+
+# Creates and calls a "Workflow" object to begin the program's operation.
+workflow = Workflow()
+workflow.display_choices()
